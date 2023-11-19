e721bae13bbdc6a9c2abee01f9a6e3570a8776fc
Enzo Giacomelli <egiacomelli09@gmail.com>
2023-11-19 17:18:30 -0300

    comandos string

---
+++ CSharp07StringMetodos/Program.cs
@@ -0,0 +1,22 @@
+﻿string texto = "       ola mundo, aula de c#  ";
+Console.WriteLine("texto original: " + texto);
+
+//int quantidadeCaracteres = texto.Length;
+Console.WriteLine("Quantidade de caracteres original: " + texto.Length);
+
+
+
+//string textoFormatado = texto.Replace(" ", "");
+texto = texto.TrimStart();
+texto = texto.TrimEnd();
+Console.WriteLine("texto formatado: " + texto);
+Console.WriteLine("Quantidade de caracteres após formatar: " + texto.Length);
+
+/*  outros comandos de string:
+texto.ToUpper();
+texto.ToLower();
+texto.StartsWith("a");
+texto.EndsWith("b");
+texto.Contains("c");
+texto.Replace("a", "b");
+*/
