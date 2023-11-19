3ef0809f01409215791f670ad1356cf14ad043d5
Enzo Giacomelli <egiacomelli09@gmail.com>
2023-11-19 18:08:13 -0300

    Adicionados exemplos de metodos de string

--- CSharp07StringMetodos/Program.cs
+++ CSharp07StringMetodos/Program.cs
@@ -20,3 +20,16 @@ texto.EndsWith("b");
 texto.Contains("c");
 texto.Replace("a", "b");
 */
+
+
+//pegando apenas o primeiro nome em uma string com o nome completo:
+string nomeCompleto = "Enzo Giacomelli";
+int indiceEspaco = nomeCompleto.IndexOf(" "); //pega a posição onde está o espaço
+string nome =  nomeCompleto.Substring(0, indiceEspaco); //pega do primeiro indice até o indice do espaço
+Console.WriteLine("\n\nNome: " + nome); //deve escrever apenas o primeiro nome
+
+//pegando o ultimo nome
+int indiceUltimoEspaco = nomeCompleto.LastIndexOf(" ");//pega o indice do ultimo espaço em caso de nome composto
+int quantidadeCaracteresUltimoNome = nomeCompleto.Length - indiceUltimoEspaco; //pega o tamanho do ultimo nome
+string ultimoNome = nomeCompleto.Substring(indiceUltimoEspaco, quantidadeCaracteresUltimoNome);// pega o texto do ultimo espaço até o fim do ultimo nome
+Console.WriteLine("Ultimo nome: " + ultimoNome);
