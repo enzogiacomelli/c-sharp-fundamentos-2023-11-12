5b4320fffcf5fd95688c9338e5e2e84862267f0f
Enzo Giacomelli <egiacomelli09@gmail.com>
2023-12-03 18:55:06 -0300

    exercicio01

---
+++ CSharp13OOMetodos/Exemplos/Exercicio01.cs
@@ -0,0 +1,32 @@
+﻿using CSharp13OOMetodos;
+
+namespace CSharp13OOMetodos.Exemplos
+{
+    internal class Exercicio01
+    {
+        public void Executar()
+        {
+            Aluno aluno1 = new Aluno();
+
+            Console.Write("Nota de logica 1: ");
+            aluno1.LogicaNota1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Nota de logica 2: ");
+            aluno1.LogicaNota2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Nota de logica 3: ");
+            aluno1.LogicaNota3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Nota de banco de dados 1: ");
+            aluno1.BancoDadosNota1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Nota de banco de dados 2: ");
+            aluno1.BancoDadosNota2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Nota de banco de dados 3: ");
+            aluno1.BancoDadosNota3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Status do aluno: \n" + aluno1.ObterStatus());
+        }
+    }
+}
--- CSharp13OOMetodos/Program.cs
+++ CSharp13OOMetodos/Program.cs
@@ -7,5 +7,8 @@
 //Exemplo02 executor02 = new Exemplo02();
 //executor02.Executar();
 
-Exemplo03 executor3 = new Exemplo03();
-executor3.Executar();
+//Exemplo03 executor3 = new Exemplo03();
+//executor3.Executar();
+
+Exercicio01 exercicio = new Exercicio01();
+exercicio.Executar();
